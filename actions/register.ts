7b000fa8fb0cec3e4d11e50e5f7f38aa6f47e548
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-09 16:54:55 +0530

    create user and post into database
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export async function register(values: z.infer<typeof RegisterSchema>) {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,23 @@ export async function register(values: z.infer<typeof RegisterSchema>) {
     return { success: false, message: validatedFields.error.message };
   }
 
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { success: false, message: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // TODO: Send email verification
   return { success: true, message: "Email sent!" };
 }
