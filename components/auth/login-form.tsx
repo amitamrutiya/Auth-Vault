4f1c267825b23147236fb2a4bfda043c2d2d2b5c
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-09 12:56:54 +0530

    create basic form and add required component
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,15 +1,59 @@
+"use client";
+
 import React from "react";
 import { CardWrapper } from "./card-wrapper";
-
+import { LoginSchema } from "@/schemas";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 export function LoginForm() {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
     <CardWrapper
       headerLabel="Welcome back!"
       backButtonLabel="Don't have an account?"
       backButtonHref="/auth/register"
       showSocial
-  >
-      LoginForm
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="john.doe@example.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage {...form.formState.errors}/>
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
   );
 }
