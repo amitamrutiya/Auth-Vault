c7eeecab5cca28e3876ffdb7d5c1038c8812287a
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-09 21:17:21 +0530

    add sign in with google and sing in with github
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,14 +3,21 @@
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { defaultLoginRedirect } from "@/route";
 
 export function Social() {
+  function onClick(provider: "google" | "github") {
+    signIn(provider, {
+      callbackUrl: defaultLoginRedirect,
+    });
+  }
   return (
     <div className="flex items-center w-full gap-x-2">
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+      <Button size="lg" className="w-full" variant="outline" onClick={() => onClick('google')}>
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+      <Button size="lg" className="w-full" variant="outline" onClick={() => onClick("github")}>
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
