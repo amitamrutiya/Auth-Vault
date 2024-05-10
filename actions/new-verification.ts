94e525464ac5a979d01050d141408abbcda0fb8d
amitamrutiya2210 <amitamrutiya2210@gmail.com>
2024-05-10 08:49:51 +0530

    add verification of email logic
    Signed-off-by: amitamrutiya2210 <amitamrutiya2210@gmail.com>

---
+++ actions/new-verification.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { getUserByEmail } from "@/data/user";
+import { getVerificationTokenByToken } from "@/data/verification-token";
+import { db } from "@/lib/db";
+
+export async function newVerification(token: string) {
+  const existingToken = await getVerificationTokenByToken(token);
+
+  if (!existingToken) {
+    return { success: false, message: "Token not found!" };
+  }
+
+  const hasExpired = new Date(existingToken.expiresAt) < new Date();
+
+  if (hasExpired) {
+    return { success: false, message: "Token has expired!" };
+  }
+
+  const user = await getUserByEmail(existingToken.email);
+
+  if (!user) {
+    return { success: false, message: "Email not found!" };
+  }
+
+  await db.user.update({
+    where: { id: user.id },
+    data: { emailVerified: new Date(), email: existingToken.email },
+  });
+
+  await db.verificationToken.delete({ where: { id: existingToken.id } });
+
+  return { success: true, message: "Email verified!" };
+}
---
+++ components/auth/new-verification-form.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { CardWrapper } from "./card-wrapper";
+import { BeatLoader } from "react-spinners";
+import { useCallback, useEffect, useState } from "react";
+import { newVerification } from "@/actions/new-verification";
+import { FormSuccess } from "../form-success";
+import { FormError } from "../form-error";
+
+export function NewVerificationForm() {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(async () => {
+    if (success || error) return;
+
+    if (!token) {
+      setError("Token not found!");
+      return;
+    }
+    try {
+      const data = await newVerification(token);
+      if (data.success) {
+        setSuccess(data.message);
+      } else {
+        setError(data.message);
+      }
+    } catch (error) {
+      setError("An error occurred!");
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (token) {
+      onSubmit();
+    }
+  });
+
+  return (
+    <CardWrapper
+      headerLabel="Confirming your verification"
+      backButtonLabel="Back to Login"
+      backButtonHref="/auth/login"
+    >
+      <div className="flex item-center w-full justify-center">
+        {!success && !error && <BeatLoader />}
+        <FormSuccess message={success} />
+        {!success && <FormError message={error} />}
+      </div>
+    </CardWrapper>
+  );
+}
